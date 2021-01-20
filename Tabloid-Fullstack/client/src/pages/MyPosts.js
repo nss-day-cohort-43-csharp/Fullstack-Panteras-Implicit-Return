3b4c2bd216f48f50879c1e5315c9b0cc63c9a7df
ste163 <samtedwards@pm.me>
2021-01-20 12:35:14 -0500

    added userProfileId to localStorage on register/login

--- Tabloid-Fullstack/client/src/pages/MyPosts.js
+++ Tabloid-Fullstack/client/src/pages/MyPosts.js
@@ -1,6 +1,7 @@
 // Authored by Sam Edwards
 // MyPosts displays all posts by the current UserId
 import React, { useState, useEffect } from "react"
+import { toast } from "react-toastify"
 
 
 const MyPosts = () => {
@@ -9,8 +10,19 @@ const MyPosts = () => {
     const [myPosts, setMyPosts] = useState([]);
 
     useEffect(() => {
-        fetch(`/api/post`)
-    })
+        debugger
+        fetch(`/api/post/getbyuserid/${userId}`)
+            .then(res => {
+                if (res.status === 404) {
+                    toast.error("Couldn't get your posts")
+                    return
+                }
+                return res.json();
+            })
+            .then(data => {
+                setMyPosts(data)
+            })
+    }, [])
 
 
     return (
