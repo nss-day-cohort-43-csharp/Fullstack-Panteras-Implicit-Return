98212ef3280dee25ec5437bb5172872af9f548ff
ste163 <samtedwards@pm.me>
2021-01-25 13:39:58 -0500

    fixed Categories only retrievable for Admins

--- Tabloid-Fullstack/client/src/pages/post/PostCreate.js
+++ Tabloid-Fullstack/client/src/pages/post/PostCreate.js
@@ -1,10 +1,43 @@
-import React from "react"
+// Authored by Sam Edwards
+// PostCreate renders Post Form
+import React, { useContext, useEffect, useState } from "react"
+import { UserProfileContext } from "../../providers/UserProfileProvider"
 
 const PostCreate = () => {
-
+    const { getToken } = useContext(UserProfileContext)
+    const [categories, setCategories] = useState([]);
+    const [newPost, setNewPost] = useState("");
+  
+    useEffect(() => {
+      getCategories();
+    }, []);
+  
+    const getCategories = () => {
+      getToken().then((token) =>
+        fetch(`/api/category`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then((res) => res.json())
+          .then((categories) => {
+            setCategories(categories);
+          })
+      );
+    };
 
     return (
+        <>
         <div>POST FORM</div>
+        <ul>
+            <li>Title</li>
+            <li>Content</li>
+            <li>CatgegoryId</li>
+            <li>Header Image URL OPTIONAL</li>
+            <li>Publication Date OPTIONAL</li>
+        </ul>
+        </>
     )
 }
 
