b21ba621a3bb38823229201e5e3af7029839e256
ste163 <samtedwards@pm.me>
2021-01-26 14:01:51 -0500

    added test for post owner being able to delete own post

--- Tabloid-Fullstack.Tests/PostTests/PostControllerTests.cs
+++ Tabloid-Fullstack.Tests/PostTests/PostControllerTests.cs
@@ -58,5 +58,38 @@ namespace Tabloid_Fullstack.Tests.PostTests
             // TotalPostCountAfterDeletion should be one less than totalPostCount
             Assert.True(totalPostCountAfterDeletion == totalPostCount - 1);
         }
+
+        [Fact]
+        public void Delete_For_Post_Owner()
+        {
+            // As the Post Owner, but not an Admin, I should be able to delete my own post.
+
+            // Get a postId that is mine
+            var postId = 1;
+
+            // Spoof an authenticated user by generating a ClaimsPrincipal
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                                        new Claim(ClaimTypes.NameIdentifier, "TEST_FIREBASE_UID_3"),
+                                   }, "TestAuthentication"));
+
+            // Spoof the PostRepository and UserProfile Repository to create a PostController
+            var postRepo = new PostRepository(_context);
+            var userProfileRepo = new UserProfileRepository(_context);
+
+            // Get full count of posts
+            var totalPostCount = postRepo.Get().Count;
+
+            // Spoof the Post Controller
+            var controller = new PostController(postRepo, userProfileRepo);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
+
+            controller.Delete(postId);
+
+            var totalPostCountAfterDeletion = postRepo.Get().Count;
+
+            // TotalPostCountAfterDeletion should be one less than totalPostCount
+            Assert.True(totalPostCountAfterDeletion == totalPostCount - 1);
+        }
     }
 }
